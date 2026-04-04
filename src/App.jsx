6d225c6f8652846c5c955c08e52525e801cd72bc
SUMANTH GODUGU <sumanth.prt@gmail.com>
2026-04-04 18:01:59 +0530

    added floating wahtsapp button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
 import Contact from "./components/Contacts";
-
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 function App() {
   const [lang, setLang] = useState("te");
 
   return (
     <>
+      <FloatingWhatsApp />
+
+
       <Navbar lang={lang} setLang={setLang} />
       <Hero lang={lang} />
       <Menu lang={lang} />
